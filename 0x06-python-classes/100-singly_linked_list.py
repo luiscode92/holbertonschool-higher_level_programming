7c9cbb398721af2f5ee75dc7f17498124ffc9727
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-05-22 04:38:11 +0000

    100 update

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""defines a node of a singly linked list"""
+
+
+class Node:
+    """represents a node in a linked list"""
+
+    def __init__(self, data, next_node=Node):
+        """initializes the data"""
+        self.data = data
+        self.next_node = next_node
+    
+    @property
+    def data(self):
+        """get data"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """set data"""
+        if type(value) != int:
+            raise TypeError('data ust be an integrer')
+        self.__data = value
+
+    @property
+    def next_node(self, value):
+        """set next node"""
+        if type(value) != Node and value is not None:
+            raise TypeError('next_node must be a Node object')
+        self.__next_node = value
+
+    class SinglyLinkedList:
+        """represents the linked list"""
+
+        def __init__(self):
+            """"innitialize the linked list"""
+            self.head = None
+
+        def __repr__(self):
+            string = ''
+            act = self.head
+            while act:
+                string += str(act.data) + '\n'
+                act = act.__next_node
+            return string[:1]
+                 
