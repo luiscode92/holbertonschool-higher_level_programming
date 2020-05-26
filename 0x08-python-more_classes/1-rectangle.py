b7e754662d121a6078dbb33cde1d120d3b417fbf
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-05-26 01:45:01 +0000

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,7 +2,7 @@
 class Rectangle:
     """Represents a rectangle."""
     def __init__(self, width=0, height=0):
-        """Initialize the data."""
+        
         self.width = width
         self.height = height
 
