1dc7c6019f83eec23bd581a96a41b0c2302cd0b4
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-05-30 00:00:30 +0000

    pep8 fixed

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -3,8 +3,10 @@
 Rectangle module
 """
 
+
 class Rectangle:
     """represents a rectangle"""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -15,20 +17,19 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
-    
+
     @property
     def height(self):
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -41,7 +42,7 @@ class Rectangle:
         total = ""
         for i in range(self.__height):
             total += ("#" * self.__width)
-            if i is not self.__height -1:
+            if i is not self.__height - 1:
                 total += "\n"
         return total
 
@@ -54,4 +55,4 @@ class Rectangle:
     def perimeter(self):
         if self.__width is 0 or self.__height is 0:
             return 0
-        return (2 * self.__width) + (2 *self.__height)                           
+        return (2 * self.__width) + (2 * self.__height)
--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
@@ -32,7 +32,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -59,4 +59,4 @@ class Rectangle:
     def perimeter(self):
         if self.__width is 0 or self.__height is 0:
             return 0
-        return (2 * self.__width) + (2 * self.__height)                        
+        return (2 * self.__width) + (2 * self.__height)
