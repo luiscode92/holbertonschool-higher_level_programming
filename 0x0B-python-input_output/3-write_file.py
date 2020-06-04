7635c6b64dc850b06557331ba7ad3d0519804e56
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-06-04 01:02:14 +0000

    update

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -7,5 +7,5 @@ write file module
 def write_file(filename="", text=""):
     """write string ina  text."""
     with open(filename, 'w') as f:
-        return f.write(str(text))
+        return f.write(text)
         
