06d63684f8ff415ca29f9f31f4c16693be600378
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-06-04 00:57:53 +0000

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ The read file module defines one function, read_file().
 
 def read_file(filename=""):
     """Print the contents of a text file."""
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         print(f.read(), end='')
