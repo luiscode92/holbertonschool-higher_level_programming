073c39d33ad764360b6c4f5bd8c4e9a62cdf8aef
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-06-05 22:03:57 +0000

    update

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+pascal triangle module
+"""
+
+
+def pascal_triangle(n):
+    """ return ist that represent pascal's triangle"""
+    my_list = []
+    if n <= 0:
+        return my_list
+    for i in range(1, n + 1):
+        value = 1
+        tmp_list = []
+        for j in range(1, i + 1):
+            tmp_list.append(str(value)) 
+            value = value * (i - j) // j
+        my_list.append(tmp_list)
+    return my_list           
