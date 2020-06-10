9428113db6662c2bdab73260b6bea78c765f5e03
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-06-10 04:59:36 +0000

    test update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,6 +16,10 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+    def __str__(self):
+        """print rectangle attributes"""
+        return ('[Rectangle] (' + str(self.id) + ') ' + str(self.__x) + '/' + str(self.__y) + ' - ' + str(self.__width) + '/' + str(self.__height))
+
     def integer_validator(self, attr, value):
         """validate integer"""
         if type(value) != int:
@@ -24,6 +28,40 @@ class Rectangle(Base):
             raise ValueError('{} must be > 0'.format(attr))
         if value < 0 and (attr == 'x' or attr == 'y'):
             raise ValueError('{} must be >=0'.format(attr))
+
+    def area(self):
+        """represents the area of a triangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """print the rectangle"""
+        print('\n' * self.__y + (' ' * self.__x + '#' * self.__width + '\n') * self.__height, end="")
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """return dictionary rep of rectangle"""
+        return {
+            'id': self.id,
+            'width': self.__width,
+            'height': self.__height,
+            'x': self.__x,
+            'y': self.__y}
 
     @property
     def width(self):
@@ -67,51 +105,4 @@ class Rectangle(Base):
     def y(self, value):
         """set the y"""
         self.integer_validator('y', value)
-        self.__y = value
-
-    def area(self):
-        """represents the area of a triangle"""
-        return self.__width * self.__height
-
-    def display(self):
-        """print the rectangle"""
-        print('\n' * self.__y + (' ' * self.__x + '#' * self.__width + '\n') * self.__height, end="")
-
-    def __str__(self):
-        """print rectangle attributes"""
-        return ('[Rectangle] (' +
-                str(self.id) +
-                ') ' +
-                str(self.__x) +
-                '/' +
-                str(self.__y) +
-                ' - ' +
-                str(self.__width) +
-                '/' +
-                str(self.__height))
-
-    def update(self, *args, **kwargs):
-        """assigns an argument to each attribute"""
-        if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.__width = args[1]
-            if len(args) >= 3:
-                self.__height = args[2]
-            if len(args) >= 4:
-                self.__x = args[3]
-            if len(args) >= 5:
-                self.__y = args[4]
-        elif kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def to_dictionary(self):
-        """return dictionary rep of rectangle"""
-        return {
-            'id': self.id,
-            'width': self.__width,
-            'height': self.__height,
-            'x': self.__x,
-            'y': self.__y}
+        self.__y = value  
