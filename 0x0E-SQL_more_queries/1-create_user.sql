cc2128b90faef3ad67403d91b2d93caa3e856564
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-07-14 22:48:24 +0000

    add create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create a new user with root privileges
+CREATE USER IF NOT EXISTS user_0d_1@localhost;
+SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+FLUSH PRIVILEGES;
