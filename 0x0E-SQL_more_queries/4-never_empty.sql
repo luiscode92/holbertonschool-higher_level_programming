a9ec06e8f5e21aa6c929a73a538a6379290b8ed6
luis sanjuan <luiscode92@gmail.com>
2020-07-15 07:29:09 -0500

    never empty

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- create the table id_not_null with id and name
-CREATE TABLE IF NOT EXISTS 'id_not_null'(id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
