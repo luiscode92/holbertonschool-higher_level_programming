eb789ed42d4de664b49fee15817452591a14b80e
luis sanjuan <luiscode92@gmail.com>
2020-07-15 07:55:45 -0500

    add uniq id

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- create the table unique id and name
-CREATE TABLEE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
