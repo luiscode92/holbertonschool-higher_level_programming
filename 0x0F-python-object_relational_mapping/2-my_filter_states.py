d4d7ccd8071b9f8d6e5aee809718c24c27565e74
Luis Sanjuan <luiscode92@gmail.com>
2020-08-08 12:31:12 -0500

    add atate filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(argv[4]))
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
+
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+write a script whre name mathces the argument
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", (argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
