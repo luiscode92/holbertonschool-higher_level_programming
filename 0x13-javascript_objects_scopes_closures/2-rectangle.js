786cb59c80aaf4f5e21a54514c7be7fcf14ecdc1
Luis Sanjuan <luiscode92@gmail.com>
2020-09-21 15:43:39 -0500

    Upload- 2 rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+modules.exports = Rectangle;
