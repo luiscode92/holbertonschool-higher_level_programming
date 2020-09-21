69aa46f1a85cf61f151c3d926af196d25d12262c
Luis Sanjuan <luiscode92@gmail.com>
2020-09-21 16:24:24 -0500

    Upload-rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+Rectangle.prototype.print = function () {
+  for (let i = 0; i < this.height; i++) {
+    console.log('X'.repeat(this.width));
+  }
+};
+
+module.exports = Rectangle;
