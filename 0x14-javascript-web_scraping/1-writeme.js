388202c9aead2b8f6cc64dac7e45ffe85db54df8
Luis Sanjuan <luiscode92@gmail.com>
2020-09-22 23:45:56 -0500

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err, data) => {
-    if (err) {
-        return console.log(err);
-    }
-})
+  if (err) {
+    return console.log(err);
+  }
+});
